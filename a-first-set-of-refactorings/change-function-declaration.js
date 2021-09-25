42105b94f054aaa329a829b98cc65d0f7b55feab
Keisuke <keisuke.yamauchi.321@gmail.com>
2021-09-25 22:40:58 +0900

    update 関数宣言の変更

--- a-first-set-of-refactorings/change-function-declaration.js
+++ a-first-set-of-refactorings/change-function-declaration.js
@@ -1,9 +1,100 @@
 function circum(radius) {
-  //
+  return 2 * Math.PI * radius;
 }
 
 function circumference(radius) {
-  //
+  return 2 * Math.PI * radius;
 }
 
 // =====================================================================================================================
+
+function circum2(radius) {
+  return circumference2(radius);
+}
+
+function circumference2(radius) {
+  return 2 * Math.PI * radius;
+}
+
+// ---------------
+
+class Book {
+  addReservation(customer) {
+    this._resevation.push(customer);
+  }
+}
+
+// ---------------
+
+class Book2 {
+  addReservation(customer) {
+    zz_addReservation(customer);
+  }
+
+  zz_addReservation(customer) {
+    this._resevation.push(customer);
+  }
+}
+
+// ---------------
+
+class Book3 {
+  addReservation(customer) {
+    zz_addReservation(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    this._resevation.push(customer);
+  }
+}
+
+// ---------------
+
+class Book4 {
+  addReservation(customer) {
+    zz_addReservation(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority === false);
+    this._resevation.push(customer);
+  }
+}
+
+// ----------------
+
+function inNewEngland(aCustomer) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+}
+
+// caller
+const newEnglanders = someCostomers.filter(c => inNewEngland(c));
+
+// -----------
+
+function inNewEngland2(aCustomer) {
+  const stateCode = aCustomer.address.state;
+  return xxNewEngland(stateCode);
+}
+
+function xxNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+// -------------
+
+function inNewEngland3(aCustomer) {
+  return xxNewEngland(aCustomer.address.state);
+}
+
+// caller
+const newEnglanders = someCustomer.filter(c => xxNewEngland(c.address.state));
+
+// ---------------
+
+// caller
+const newEnglanders = someCustomer.filter(c => inNewEngland4(c.address.state));
+
+function inNewEngland4(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
