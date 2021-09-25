3b06dd543182cf1ab6c84949ee1d41434746bbef
Keisuke <keisuke.yamauchi.321@gmail.com>
2021-09-25 22:44:35 +0900

    add 変数のカプセル化

---
+++ a-first-set-of-refactorings/encapsulate-variable.js
@@ -0,0 +1,8 @@
+let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
+
+let defaultOwnerData = {firstName: "Martin", lastName: "Fowler"};
+export  function defaultOwner2() {return defaultOwnerData;}
+export function setDefaultOwner(arg) {defaultOwnerData = arg;}
+
+// =====================================================================================================================
+
