9de72510e9fc211e1643bc44632fb8eff4019c5f
Keisuke <keisuke.yamauchi.321@gmail.com>
2021-09-25 21:48:07 +0900

    add 変数のインライン化

---
+++ a-first-set-of-refactorings/inline-variable.js
@@ -0,0 +1,11 @@
+function example1() {
+  let basePrice = anOrder.basePrice;
+  return (basePrice > 1000);
+}
+
+function example2() {
+  return anOrder.basePrice > 1000;
+}
+
+// =====================================================================================================================
+
