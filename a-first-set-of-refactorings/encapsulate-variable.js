6f2ec4f8e2b68b0ab47427ac21dc9e4ac2bff34c
Keisuke <keisuke.yamauchi.321@gmail.com>
2021-09-25 23:05:23 +0900

    update 変数のカプセル化

--- a-first-set-of-refactorings/encapsulate-variable.js
+++ a-first-set-of-refactorings/encapsulate-variable.js
@@ -6,3 +6,45 @@ export function setDefaultOwner(arg) {defaultOwnerData = arg;}
 
 // =====================================================================================================================
 
+let defaultOwner3 = {firstName: "Martin", lastName: "Fowler"};
+
+// caller
+spaceship.owner = defaultOwner3;
+
+// setter
+defaultOwner3 = {firstName: "Rebecca", lastName: "Parsons"};
+
+// ------------
+
+function getDefaultOwner() {return defaultOwner3;}
+function setDefaultOwner2(arg) {defaultOwner3 = arg;}
+
+spaceship.owner = getDefaultOwner();
+
+setDefaultOwner2({firstName: "Rebecca", lastName: "Parsons"});
+
+// --------------
+
+const owner1 = getDefaultOwner();
+assert.equal("Fowler", owner1.lastName, "when set");
+const owner2 = getDefaultOwner();
+owner2.lastName = "Parsons";
+assert.equal("Parsons", owner1.lastName, "after change owner2"); // これで良い？
+
+// -----------
+
+let defaultOwnerData2 = {firstName: "Martin", lastName: "Fowler"};
+export function defaultOwner4() {return Object.assign({}, defaultOwnerData2);}
+export function setDefaultOwner3(arg) {defaultOwnerData2 = arg;}
+
+// --------------
+
+export function defaultOwner5() {return new Person(defaultOwnerData2);}
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+  get lastName() {return this._lastName;}
+  get firstName() {return this._firstName; }
+}
