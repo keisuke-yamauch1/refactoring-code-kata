437f94a809b9f92a66bd001a0ee5f1927a45ea18
Keisuke <keisuke.yamauchi.321@gmail.com>
2021-09-26 11:43:23 +0900

    update 変数名の変更

--- a-first-set-of-refactorings/rename-variable.js
+++ a-first-set-of-refactorings/rename-variable.js
@@ -3,3 +3,31 @@ let a = height * width;
 let area = height * width;
 
 // =====================================================================================================================
+
+let tpHd = "untitled";
+
+// 参照
+result += `<h1>${tpHd}</h1>`;
+
+// 更新
+tpHd = obj['articleTitle'];
+
+// ------------
+
+result += `<h1>${title()}</h1>`;
+
+setTitle(obj['articleTitle']);
+
+function title() {return tpHd;}
+function setTitle(arg) {tpHd = arg;}
+
+let _title = "untitled";
+
+function title2() {return _title;}
+function setTitle2(arg) {_title = arg;}
+
+// -------------
+const cpyNm = "Acme Gooseberries";
+
+const companyName = "Acme Gooseberries";
+const cpyNm2 = companyName;
